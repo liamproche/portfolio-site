9cd50d5b1370584b8549c589886232293f6aabe9
Liam Roche <roche.liamp@gmail.com>
2022-02-25 15:59:28 -0500

    nav on landing page fades in when user clicks text

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 console.log('hello')
+const $landingPageText = $('.landing-page-text')
+const $navLinksContainer = $('#nav-links-container')
+
+
+
 $('.description-box').on('click', (e)=>{
     $(e.target).parent().toggleClass('show-description')
 })
 
-
+$($landingPageText).on('click', ()=>{
+    $('#nav-links-container').css('opacity', 1)
+})
 
