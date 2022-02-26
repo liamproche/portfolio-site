86d95ed43ed6e6adc08729fbef2976a850b89766
Liam Roche <roche.liamp@gmail.com>
2022-02-26 14:33:12 -0500

    mobile nav working on resume site

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-console.log('hello')
-const $landingPageText = $('.landing-page-text')
-const $navLinksContainer = $('#nav-links-container')
+console.log('hello');
+const $landingPageText = $('.landing-page-text');
+const $navLinksContainer = $('#nav-links-container');
+const $hamburger = $('.menu-image');
+const $sideNav = $('.side-nav');
+const $x = $('.x')
 
 
 
 $('.description-box').on('click', (e)=>{
-    $(e.target).parent().toggleClass('show-description')
-})
+    $(e.target).parent().toggleClass('show-description');
+});
 
 $($landingPageText).on('click', ()=>{
-    $('#nav-links-container').css('opacity', 1)
-    
-})
+    $('#nav-links-container').css('opacity', 1);  
+});
 
+$($hamburger).on('click', ()=>{
+    $($sideNav).toggleClass('show-nav');
+});
+$($x).on('click', ()=>{
+    $($sideNav).toggleClass('show-nav')
+})
