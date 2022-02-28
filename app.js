442b078ff485b33b0d686eeac42e380e01293032
Liam Roche <roche.liamp@gmail.com>
2022-02-27 23:39:48 -0500

    fixed bug in project sliding panels

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
+//checks javascript is working
+
 console.log('hello');
-const $landingPageText = $('.landing-page-text');
+
+// const $landingPageText = $('.landing-page-text');
 const $navLinksContainer = $('#nav-links-container');
 const $hamburger = $('.menu-image');
 const $sideNav = $('.side-nav');
 const $x = $('.x')
 const $textBox = $('.text-box');
 const $projectBox = $('.project-container');
-const $projectText = $('.project-text')
+const $projectText = $('.project-text');
 
 
 
-
-$('.description-box').on('click', (e)=>{
-    $(e.target).parent().toggleClass('show-description');
-});
-
-$($landingPageText).on('click', ()=>{
-    $('#nav-links-container').css('opacity', 1);  
-});
+//START NAV MENU
 
 $($hamburger).on('click', ()=>{
     $($sideNav).toggleClass('show-nav');
@@ -27,32 +23,46 @@ $($hamburger).on('click', ()=>{
 $($x).on('click', ()=>{
     $($sideNav).toggleClass('show-nav');
     $($hamburger).css('opacity', '1');
-})
+});
+
+//END NAV MENU
 
 
-
-
-function changeQuestionMark(){
-$('.hero h1').on('click', function(){
-        console.log('clicked')
-        // GET BETTER FONT FOR THIS
-        $('.punctuation').text('. :-)')
-        nameAppear();
-        jobAppear();
-    })
-}
-
-function nameAppear(){
-    $('.hero h2').toggleClass('second-appear');
-}
-
-function jobAppear(){
-    $('.hero h3').toggleClass('third-appear');
-}
-
-changeQuestionMark();
-
+//PROJECTS SLIDER PANELS START
 $($projectBox).on('click', (e)=>{
     $(e.target).toggleClass('show-description');
     console.log('clicked');
+});
+$($projectText).on('click', (e)=>{
+    $(e.target).parent().parent().toggleClass('show-decription')
 })
+
+
+//LANDING PAGE OPENING SEQUENCE START
+function changeQuestionMark(){
+    $('.hero h1').on('click', function(){
+            console.log('clicked')
+            // GET BETTER FONT FOR THIS
+            $('.punctuation').text('. :-)')
+            nameAppear();
+            jobAppear();
+    })
+}
+
+
+//for name appear effect
+function nameAppear(){
+    $('.hero h2').toggleClass('second-appear');
+};
+
+
+//for job appear effect
+function jobAppear(){
+    $('.hero h3').toggleClass('third-appear');
+};
+
+   
+//calls first event in landing page sequence
+changeQuestionMark();
+
+//LANDING PAGE OPENING SEQUENCE END
